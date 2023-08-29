aaad322feda5380c8b9a1bce7b6ced08f20b8e07
Mamun172136 <ma1536204@gmail.com>
2023-08-30 01:16:53 +0600

    [done] AcademicDepartmentRoutes for getAllfromdb and insertdb

--- src/app/modules/academicDepartment/academicDepartment.route.ts
+++ src/app/modules/academicDepartment/academicDepartment.route.ts
@@ -5,6 +5,12 @@ import { AcademicDepartmentValidation } from './academicDepartment.validation';
 
 const router = express.Router();
 
+router.get(
+  '/',
+
+  AcademicDepartmentController.getAllFromDB
+);
+
 router.post(
   '/',
   validateRequest(AcademicDepartmentValidation.create),
