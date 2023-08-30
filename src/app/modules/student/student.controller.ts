089e41f4f64b2d59a8cb7fb481f7c14a9c73bc25
Mamun172136 <ma1536204@gmail.com>
2023-08-30 22:11:21 +0600

    [done] student controller updateIntodb

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { StudentService } from './student.service';
+
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const payload = req.body;
+  const result = await StudentService.updateIntoDB(id, payload);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student updated successfully',
+    data: result,
+  });
+});
+
+export const StudentController = {
+  updateIntoDB,
+};
